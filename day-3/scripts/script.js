e82fae620e16b185a1ea939cd117d931dc9ed291
HyOcee <eosemegbe@gmail.com>
2022-02-16 16:12:29 +0100

    hhfh

--- day-3/scripts/script.js
+++ day-3/scripts/script.js
@@ -1,18 +1,23 @@
-let res = prompt('who is there?', 'Jae Hee');
+// let res = prompt('who is there?', 'Jae Hee');
 
-if(res === null) {
-    console.log('cancelled')
-} else if(res !== 'Admin') {
-    console.log('I dont know you')
-} else {
-    let password = prompt('Enter Password');
+// if(res === null) {
+//     console.log('cancelled')
+// } else if(res !== 'Admin') {
+//     console.log('I dont know you')
+// } else {
+//     let password = prompt('Enter Password');
 
-    if(password === null) {
-        console.log('cancelled')
-    } else if(password !== 'TheMaster') {
-        console.log('wrong password')
-    } else {
-     console.log('You\'re welcome boss')
-    }
+//     if(password === null) {
+//         console.log('cancelled')
+//     } else if(password !== 'TheMaster') {
+//         console.log('wrong password')
+//     } else {
+//      console.log('You\'re welcome boss')
+//     }
+// }
+
+let dataIn = document.getElementsByTagName('input');
+
+for (let i = 0; i < dataIn.length; i++) {
+    console.log(dataIn[i].value)
 }
-
